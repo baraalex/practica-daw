da669b0b933370b33142bd0e5ad58ed84341e49c
Sergio Conde Gómez <skgsergio@gmail.com>
2015-05-14 21:27:21 +0200

    Python 2 de las narices...

--- proyecto/web/views.py
+++ proyecto/web/views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from django.shortcuts import render, redirect
 from django.views.decorators.http import require_GET  # , require_POST
 # from django.views.decorators.http import require_http_methods
