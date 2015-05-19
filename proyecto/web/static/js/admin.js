2c6837f31e1dceccf06c1784d888fdd6aae36790
Sergio Conde Gómez <skgsergio@gmail.com>
2015-05-19 21:29:59 +0200

    Union de cambios

--- proyecto/web/static/js/admin.js
+++ proyecto/web/static/js/admin.js
@@ -93,7 +93,7 @@ function addJugador() {
 function addJugadores(nameEquipo) {
 
     var msg =
-        "<span id=\"nameaux\"><h4>Jugadores :</h4></span><div class='table-responsive' style='max-height: 20em;overflow: auto;'>" +
+        "<span id=\"nameaux\"><h4>Jugadores :</h4></span><div class='table-responsive jornada'>" +
         "<table class='table table-striped table-bordered'><thead><tr><th>Nombre</th><th>Dorsal</th><th>Borrar</th></tr></thead>" +
         "<tbody id='tablebody'></tbody></table></div> ";
 
@@ -124,35 +124,54 @@ function addJugadores(nameEquipo) {
 }
 
 function addEquipos(nameLiga) {
-
     var msg =
-        "<span id=\"nameaux\"><h4>Equipos :</h4></span><div class='table-responsive' style='max-height: 20em;overflow: auto;'>" +
+        "<div class='row'><div class='col-md-6'><span id=\"nameaux\"><h4>Equipos :</h4></span><div class='table-responsive jornada'>" +
         "<table class='table table-striped table-bordered'><thead><tr><th>Nombre</th><th>Borrar</th></tr></thead>" +
-        "<tbody id='tablebody'></tbody></table></div> ";
+        "<tbody id='tablebodyAdded'></tbody>" +
+        "</table></div></div><div class='col-md-6'><span id=\"nameaux\">" +
+        "<h4>Equipos :</h4></span><div class='table-responsive jornada'>" +
+        "<table class='table table-striped table-bordered'><thead><tr><th>Nombre</th><th>Add</th></tr></thead>" +
+        "<tbody id='tablebody'><tr id='add2'><td>E2</td><td class='butt'><span class='fa fa-plus'></span></td></tr>" +
+        "<tr id='add3'><td>E3</td><td class='butt'><span class='fa fa-plus'></span></td></tr>" +
+        "<tr id='add4'><td>E4</td><td class='butt'><span class='fa fa-plus'></span></td></tr></tbody>" +
+        "</table></div></div> </div>";
 
     bootbox.dialog({
         closeButton: false,
-        title: "Equipo: " + nameLiga,
+        title: "Add equipos a liga: " + nameLiga,
         message: msg,
         buttons: {
-            add: {
-                label: '<span class="fa fa-plus" aria-hidden="true"></span>',
-                className: "btn-primary",
-                callback: function () {
-                    addJugador();
-                    e.preventDefault();
-                    return false;
-                    //A�adir jugador
-                }
-            }, ok: {
+            ok: {
                 label: '<span class="fa fa-check" aria-hidden="true"></span>',
-                className: "btn-success"
+                className: "btn-success",
+                callback: function () {
+                    var add = document.getElementById("tablebodyAdded").children;
+                    var all = "";
+                    for (i = 0; i < add.length; i++) {
+                        all = all + add[i].id + ",";
+                    }
+                    alert(all);
+                }
             },
             cancel: {
                 label: '<span class="fa fa-remove" aria-hidden="true"></span>',
                 className: "btn-danger"
             }
         }
+    });
+
+    $(".butt").click(function () {
+        var el = $(this);
+        $("#tablebodyAdded").append("<tr id='" + el[0].parentNode.id.split("add")[1] + "'><td>" +
+            el[0].parentNode.childNodes[0].childNodes[0].data +
+            "</td><td class='butt-danger'><span class='fa fa-minus'></span></td></tr>");
+        el.addClass('disabled');
+        $(".butt-danger").click(function () {
+            //alert("#add" + $(this)[0].parentNode.id);
+            document.getElementById("add" + $(this)[0].parentNode.id).children[1].setAttribute("class", "butt");
+            $(this)[0].parentNode.remove();
+        });
+        //jornada($(this).html());
     });
 }
 
@@ -178,12 +197,12 @@ function change(bool) {
             '<th colspan="5" class="text-center name">Jornadas</th></tr></thead><tbody id="jornadastable">' +
             '<tr><td class="day old">5</td><td class="day">6</td><td class="day">7</td><td class="day">8</td>' +
             '<td class="day">9</td></tr></tbody> </table></div>');
-            Sortable.init();
-            $(".day").click(function () {
-                $('.day').removeClass('active');
-                $(this).addClass('active');
-                jornada($(this).html());
-            });
+        Sortable.init();
+        $(".day").click(function () {
+            $('.day').removeClass('active');
+            $(this).addClass('active');
+            jornada($(this).html());
+        });
 
     } else {
         $("#addbtt").empty();
@@ -209,7 +228,7 @@ function change(bool) {
 
 function jornada(jor) {
     var msg =
-        "<span id=\"nameaux\"><h4>Partidos :</h4></span><div class='jornada'>" +
+        "<span id=\"nameaux\"><h4>Partidos :</h4></span><div class='table-responsive jornada'>" +
         "<table class='table table-striped table-bordered dataTable sortable-theme-bootstrap'' id='jornadaTable'><thead><tr><th>editar</th><th>Local</th>" +
         "<th>Visitante</th><th>Resultado</th></tr></thead>" +
         "<tbody id='tablebody'><tr><td class='butt' onclick='partido(0,1);'><span class='fa fa-pencil-square-o'></span></td>" +
@@ -234,18 +253,18 @@ function jornada(jor) {
 }
 
 function partido(id, pos) {
-    var msg = "<div  id ='alert' style='display:none;'' class='alert alert-warning alert-dismissible' role='alert'>"+
-    "<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>"+
-  "<strong>Error!</strong>goles negativos</div>"    +
+    var msg = "<div  id ='alert' style='display:none;'' class='alert alert-warning alert-dismissible' role='alert'>" +
+        "<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button>" +
+        "<strong>Error!</strong>goles negativos</div>" +
         "<div class='row row-right'><div class='col-md-6'> <span id='campoaux'><h4>Local :</h4></span>" +
         "<li>Goles: <span id='golLocalTotal' class='badge gol'>0</li><li>Estadisticas:" +
-        "<div class='jornada'><table><thead><tr><th>Nombre</th><th>Dorsal</th><th>Posicion</th><th>Amarillas</th><th>Rojas</th><th>Goles</th><th>Goles PP</th></tr></thead>" +
+        "<div class='table-responsive jornada'><table><thead><tr><th>Nombre</th><th>Dorsal</th><th>Posicion</th><th>Amarillas</th><th>Rojas</th><th>Goles</th><th>Goles PP</th></tr></thead>" +
         "<tbody><tr><td>Jugador</td><td>15</td><td>MC</td><td><input type='number' min='0' max='2' value='0' style='width: 5em;'></td><td>" +
         "<input type='checkbox'></td><td><input name='golLocal' type='number' min='0' max='100' value='0' style='width: 5em;' onchange='calcGol();'required=''></td>" +
         "<td><input name='golLocalpp' type='number' min='0' max='100' value='0' style='width: 5em;' onchange='calcGol();'required=''></td></tr></tbody></table></div></li>" +
         "</div><div class='col-md-6'><span id=\"neq\"><h4>Visitante :</h4></span>" +
         "<li>Goles: <span id='golVisitanteTotal' class='badge gol'>0</span></li><li>Estadisticas:" +
-        "<div class='jornada'><table><thead><tr><th>Nombre</th><th>Dorsal</th><th>Posicion</th><th>Amarillas</th><th>Rojas</th><th>Goles</th><th>Goles PP</th></tr></thead>" +
+        "<div class='table-responsive jornada'><table><thead><tr><th>Nombre</th><th>Dorsal</th><th>Posicion</th><th>Amarillas</th><th>Rojas</th><th>Goles</th><th>Goles PP</th></tr></thead>" +
         "<tbody><tr><td>Jugador</td><td>15</td><td>MC</td><td><input type='number' min='0' max='2' value='0' style='width: 5em;'></td><td>" +
         "<input type='checkbox'></td><td><input name='golVisitante' type='number' min='0' max='100' value='0' style='width: 5em;' onchange='calcGol();' required=''></td>" +
         "<td><input type='number' name='golVisitantepp' min='0' max='100' value='0' style='width: 5em;' onchange='calcGol();'required=''></td></tr></tbody></table></div></li></div></div>";
@@ -259,44 +278,44 @@ function partido(id, pos) {
                 label: '<span class="fa fa-check" aria-hidden="true"></span>',
                 className: "btn-success",
                 callback: function (e) {
-                var local = document.getElementsByName("golLocal");
-                var localpp = document.getElementsByName("golLocalpp");
-                var visitante = document.getElementsByName("golVisitante");
-                var visitantepp = document.getElementsByName("golVisitantepp");
-                var loc = 0;
-                var vis = 0;
+                    var local = document.getElementsByName("golLocal");
+                    var localpp = document.getElementsByName("golLocalpp");
+                    var visitante = document.getElementsByName("golVisitante");
+                    var visitantepp = document.getElementsByName("golVisitantepp");
+                    var loc = 0;
+                    var vis = 0;
 
-                for(i=0;i<local.length;i++){
-                    if(parseInt(local[i].value)<0){
-                        $("#alert").css({"display":"block"});
-                        e.preventDefault();
-                        return false;
+                    for (i = 0; i < local.length; i++) {
+                        if (parseInt(local[i].value) < 0) {
+                            $("#alert").css({"display": "block"});
+                            e.preventDefault();
+                            return false;
+                        }
                     }
-                }
 
-                for(i=0;i<visitantepp.length;i++){
-                    if(parseInt(visitantepp[i].value)<0){
-                        $("#alert").css({"display":"block"});
-                        e.preventDefault();
-                        return false;
+                    for (i = 0; i < visitantepp.length; i++) {
+                        if (parseInt(visitantepp[i].value) < 0) {
+                            $("#alert").css({"display": "block"});
+                            e.preventDefault();
+                            return false;
+                        }
                     }
-                }
 
-                for(i=0;i<visitante.length;i++){
-                    if(parseInt(visitante[i].value)<0){
-                        $("#alert").css({"display":"block"});
-                        e.preventDefault();
-                        return false;
+                    for (i = 0; i < visitante.length; i++) {
+                        if (parseInt(visitante[i].value) < 0) {
+                            $("#alert").css({"display": "block"});
+                            e.preventDefault();
+                            return false;
+                        }
                     }
-                }
 
-                for(i=0;i<localpp.length;i++){
-                    if(parseInt(localpp[i].value)<0){
-                    $("#alert").css({"display":"block"});
-                        e.preventDefault();
-                        return false;
+                    for (i = 0; i < localpp.length; i++) {
+                        if (parseInt(localpp[i].value) < 0) {
+                            $("#alert").css({"display": "block"});
+                            e.preventDefault();
+                            return false;
+                        }
                     }
-                }
 
                     var goles = $("#golLocalTotal").html() + "-" + $("#golVisitanteTotal").html();
                     $("#res" + pos).empty();
@@ -311,7 +330,6 @@ function partido(id, pos) {
     });
 }
 
-
 function calcGol() {
 
     var local = document.getElementsByName("golLocal");
@@ -321,27 +339,31 @@ function calcGol() {
     var loc = 0;
     var vis = 0;
 
-    for(i=0;i<local.length;i++){
-        if(parseInt(local[i].value)<0){
-            $("#alert").css({"display":"block"});}
+    for (i = 0; i < local.length; i++) {
+        if (parseInt(local[i].value) < 0) {
+            $("#alert").css({"display": "block"});
+        }
         loc = loc + parseInt(local[i].value);
     }
 
-    for(i=0;i<visitantepp.length;i++){
-        if(parseInt(visitantepp[i].value)<0){
-            $("#alert").css({"display":"block"});}
+    for (i = 0; i < visitantepp.length; i++) {
+        if (parseInt(visitantepp[i].value) < 0) {
+            $("#alert").css({"display": "block"});
+        }
         loc = loc + parseInt(visitantepp[i].value);
     }
 
-    for(i=0;i<visitante.length;i++){
-        if(parseInt(visitante[i].value)<0){
-            $("#alert").css({"display":"block"});}
+    for (i = 0; i < visitante.length; i++) {
+        if (parseInt(visitante[i].value) < 0) {
+            $("#alert").css({"display": "block"});
+        }
         vis = vis + parseInt(visitante[i].value);
     }
 
-    for(i=0;i<localpp.length;i++){
-        if(parseInt(localpp[i].value)<0){
-            $("#alert").css({"display":"block"});}
+    for (i = 0; i < localpp.length; i++) {
+        if (parseInt(localpp[i].value) < 0) {
+            $("#alert").css({"display": "block"});
+        }
         vis = vis + parseInt(localpp[i].value);
     }
 
